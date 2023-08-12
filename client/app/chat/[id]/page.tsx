6d25f2bd2ea07bf8ec6bd025053319f11000be41
Ankur Gupta <ankurgupta.bt21cse@pec.edu.in>
2023-08-12 15:56:57 +0530

    #Frontend + SignIn

---
+++ client/app/chat/[id]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ChatPage() {
+  return (
+    <div>
+      ChatPage
+    </div>
+  )
+}
+
+export default ChatPage
--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images:{
+    domains:['e0.pxfuel.com']
+  },
+  experimental :{
+    appDir : true,
+  },
+  
 }
 
 module.exports = nextConfig
