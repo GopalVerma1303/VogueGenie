5eb3171b9a78b1d1cad78a8b519e0cf4b0e32eb3
Gopal Verma <gopalverma@Gopals-MacBook-Air.local>
2023-08-14 17:29:36 +0530

    path for next-auth corrected

---
+++ client/assets/theme/colors.ts
@@ -0,0 +1,13 @@
+const Colors = {
+    darkyellow : "#F5A701",
+    yellow: "#F9E631",
+    darkpurple: "#463EDA",
+    purple: "#6B5CF8",
+    orange: "#FB641B",
+    flipkartblue: "#466CEA",
+    bgwhite: "#F0F3F6",
+    fgwhite: "#FEFEFF"
+
+}
+
+export default Colors;
