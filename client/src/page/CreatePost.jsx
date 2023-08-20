e03b180c1ca25ab24a7d3525d0d21c4f0d3026d6
Gopal Verma <gopalverma@Gopals-MacBook-Air.local>
2023-08-20 14:05:50 +0530

    updated chatpage

--- client/src/page/CreatePost.jsx
+++ client/src/page/CreatePost.jsx
@@ -173,8 +173,28 @@ const CreatePost = () => {
           </div>
         </div>
         <div className="w-4/5 h-full p-3">
-          <div className=' h-full bg-white rounded-2xl shadow-lg'>
+          <div className=' h-full bg-white rounded-2xl shadow-lg flex flex-col justify-between p-3'>
+            <div className=' flex flex-col overflow-y-auto h-[calc(100vh-235px)] my-5 -mt-0.5'>
+              {[...Array(100)].map((_, index) => (
+                <div className='rounded-lg py-1 text-sm text-[#666e75]' key={index}>This is div {index + 1}</div>
+              ))}
+            </div>
+            <div className=' flex px-5 py-4 bg-[#e6ebf4] rounded-xl'>
+              <form className='flex space-x-5 p-0 justify-center items-center flex-1'>
+                <input
+                  className="bg-transparent focus:outline-none flex-1 disabled:cursor-not-allowed disabled:text-gray-300"
+                  type="text"
+                  placeholder="Enter a prompt here"
+                />
+                <button
+                  className="flex bg-[#6469ff] hover:opacity-50 text-white font-bold px-2 py-2 rounded-lg disabled:bg-gray-300 disabled:cursor-not-allowed"
+                  type="submit"
+                >
+                  {/* <PaperAirplaneIcon className="w-5 h-5" /> */} Send
+                </button>
 
+              </form>
+            </div>
           </div>
         </div>
       </div>
