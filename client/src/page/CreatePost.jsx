58741fd1c60185cf51bc951c92d29e560b543be7
Gopal Verma <gopalverma@Gopals-MacBook-Air.local>
2023-08-20 13:15:14 +0530

    start added chatbox page

--- client/src/page/CreatePost.jsx
+++ client/src/page/CreatePost.jsx
@@ -78,79 +78,109 @@ const CreatePost = () => {
   };
 
   return (
-    <section className="max-w-7xl mx-auto">
-      <div>
-        <h1 className="font-extrabold text-[#222328] text-[32px]">Create</h1>
-        <p className="mt-2 text-[#666e75] text-[14px] max-w-[500px]">Generate an imaginative image through DALL-E AI and share it with the community</p>
-      </div>
+    // <section className="max-w-7xl mx-auto">
+    //   <div>
+    //     <h1 className="font-extrabold text-[#222328] text-[32px]">Create</h1>
+    //     <p className="mt-2 text-[#666e75] text-[14px] max-w-[500px]">Generate an imaginative image through DALL-E AI and share it with the community</p>
+    //   </div>
 
-      <form className="mt-16 max-w-3xl" onSubmit={handleSubmit}>
-        <div className="flex flex-col gap-5">
-          <FormField
-            labelName="Your Name"
-            type="text"
-            name="name"
-            placeholder="Ex., john doe"
-            value={form.name}
-            handleChange={handleChange}
-          />
+    //   <form className="mt-16 max-w-3xl" onSubmit={handleSubmit}>
+    //     <div className="flex flex-col gap-5">
+    //       <FormField
+    //         labelName="Your Name"
+    //         type="text"
+    //         name="name"
+    //         placeholder="Ex., john doe"
+    //         value={form.name}
+    //         handleChange={handleChange}
+    //       />
 
-          <FormField
-            labelName="Prompt"
-            type="text"
-            name="prompt"
-            placeholder="An Impressionist oil painting of sunflowers in a purple vase…"
-            value={form.prompt}
-            handleChange={handleChange}
-            isSurpriseMe
-            handleSurpriseMe={handleSurpriseMe}
-          />
+    //       <FormField
+    //         labelName="Prompt"
+    //         type="text"
+    //         name="prompt"
+    //         placeholder="An Impressionist oil painting of sunflowers in a purple vase…"
+    //         value={form.prompt}
+    //         handleChange={handleChange}
+    //         isSurpriseMe
+    //         handleSurpriseMe={handleSurpriseMe}
+    //       />
 
-          <div className="relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center">
-            { form.photo ? (
-              <img
-                src={form.photo}
-                alt={form.prompt}
-                className="w-full h-full object-contain"
-              />
-            ) : (
-              <img
-                src={preview}
-                alt="preview"
-                className="w-9/12 h-9/12 object-contain opacity-40"
-              />
-            )}
+    //       <div className="relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center">
+    //         { form.photo ? (
+    //           <img
+    //             src={form.photo}
+    //             alt={form.prompt}
+    //             className="w-full h-full object-contain"
+    //           />
+    //         ) : (
+    //           <img
+    //             src={preview}
+    //             alt="preview"
+    //             className="w-9/12 h-9/12 object-contain opacity-40"
+    //           />
+    //         )}
 
-            {generatingImg && (
-              <div className="absolute inset-0 z-0 flex justify-center items-center bg-[rgba(0,0,0,0.5)] rounded-lg">
-                <Loader />
+    //         {generatingImg && (
+    //           <div className="absolute inset-0 z-0 flex justify-center items-center bg-[rgba(0,0,0,0.5)] rounded-lg">
+    //             <Loader />
+    //           </div>
+    //         )}
+    //       </div>
+    //     </div>
+
+    //     <div className="mt-5 flex gap-5">
+    //       <button
+    //         type="button"
+    //         onClick={generateImage}
+    //         className=" text-white bg-green-700 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+    //       >
+    //         {generatingImg ? 'Generating...' : 'Generate'}
+    //       </button>
+    //     </div>
+
+    //     <div className="mt-10">
+    //       <p className="mt-2 text-[#666e75] text-[14px]">** Once you have created the image you want, you can share it with others in the community **</p>
+    //       <button
+    //         type="submit"
+    //         className="mt-3 text-white bg-[#6469ff] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+    //       >
+    //         {loading ? 'Sharing...' : 'Share with the Community'}
+    //       </button>
+    //     </div>
+    //   </form>
+    // </section>
+    // );
+    <section className="h-[calc(100vh-140px)]">
+      <div className="flex h-full">
+        <div className=" w-1/5 h-full p-3">
+          <div className=' h-full rounded-2xl p-2 justify-between flex flex-col'>
+            <div>
+              <div className=' bg-[#6469ff] justify-center items-center flex rounded-lg py-2 text-white hover:cursor-pointer'>
+                <p>+ New Chat</p>
               </div>
-            )}
+              <div className='my-3 px-2  overflow-y-auto h-[calc(100vh-290px)] '>
+                {[...Array(10)].map((_, index) => (
+                  <div className='rounded-lg py-1 text-sm text-[#666e75]' key={index}>This is div {index + 1}</div>
+                ))}
+              </div>
+            </div>
+            <div>
+              <div className=' border border-[#6469ff] justify-center items-center flex rounded-lg py-2 text-[#6469ff] hover:cursor-pointer'>
+                <p>Options</p>
+              </div>
+            </div>
           </div>
         </div>
+        <div className="w-4/5 h-full p-3">
+          <div className=' h-full bg-white rounded-2xl shadow-lg'>
 
-        <div className="mt-5 flex gap-5">
-          <button
-            type="button"
-            onClick={generateImage}
-            className=" text-white bg-green-700 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
-          >
-            {generatingImg ? 'Generating...' : 'Generate'}
-          </button>
+          </div>
         </div>
-
-        <div className="mt-10">
-          <p className="mt-2 text-[#666e75] text-[14px]">** Once you have created the image you want, you can share it with others in the community **</p>
-          <button
-            type="submit"
-            className="mt-3 text-white bg-[#6469ff] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
-          >
-            {loading ? 'Sharing...' : 'Share with the Community'}
-          </button>
-        </div>
-      </form>
+      </div>
     </section>
-  );
+
+  )
 };
 
 export default CreatePost;
